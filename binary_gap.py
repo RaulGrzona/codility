771b5467902592f6e2b5cc340740953c93dcd696
Tomi <tomi@MacBook-Pro-de-Tomi.local>
2023-04-26 17:52:21 -0300

    cambio raul x1

--- binary_gap.py
+++ binary_gap.py
@@ -1,6 +1,6 @@
 def bin_gap(n):
     bi = format(n,"0b")
-    cont = 0
+    cont = 1
     maxi = 0
     for s in range(bi.find("1"),bi.rfind("1")): #len(bi)):
         if bi[s]=="1":
@@ -12,3 +12,4 @@ def bin_gap(n):
     return(maxi)
 
 print(bin_gap(14352323423450))
+#  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
