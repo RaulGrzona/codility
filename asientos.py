3d9ec29f8a5e27567ab0c52c454636e392c03f82
Migue Vargas <mvargas1@hchb.com>
2023-05-02 17:15:50 -0300

    Added asientos file

---
+++ asientos.py
@@ -0,0 +1,8 @@
+class Asiento:
+    def __init__(self, marca_tapizado, color, material):
+        self.marca_tapizado = marca_tapizado
+        self.color = color
+        self.material = material
+
+
+
