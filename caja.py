e1b53579c0189f29ca2464d148ab4f4a298ded05
Migue Vargas <mvargas1@hchb.com>
2023-05-02 17:18:31 -0300

    Added str method to caja file

--- caja.py
+++ caja.py
@@ -1,3 +1,6 @@
 class CajaCambios:
     def __init__(self, cantidad_cambios):
         self.cantidad_cambios = cantidad_cambios
+
+    def __str__(self):
+        return f"Esta es una caja con : {self.cantidad_cambios}"
