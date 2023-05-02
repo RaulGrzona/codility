bfd91a69e8e3c40ab55d14121b8a6600678e5a3a
Migue Vargas <mvargas1@hchb.com>
2023-05-02 17:04:06 -0300

    Updated ruedas file

--- ruedas.py
+++ ruedas.py
@@ -1,11 +1,22 @@
+class Tuerca:
+    def __init__(self, marca):
+        self.marca = marca
+
+    def __str__(self):
+        return f"Marca: {self.marca}"
+
+
 class Rueda:
     def __init__(self, numero_llanta, tipo_rodado):
         self.numero_llanta = numero_llanta
         self.tipo_rodado = tipo_rodado
+        self.tuercas = [Tuerca(marca="pirelli")] * 5
 
     def __str__(self):
         return (
-            f"Numero de llanta: {self.numero_llanta}- Tipo rodado: {self.tipo_rodado}"
+            f"Numero de llanta: {self.numero_llanta} -"
+            f" Tipo rodado: {self.tipo_rodado} -"
+            f"Tuercas: {self.tuercas}"
         )
 
 
