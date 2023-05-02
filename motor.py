0c73689ae89245b6586c1552beef043f59372c69
Migue Vargas <mvargas1@hchb.com>
2023-05-02 17:21:08 -0300

    Added cilindradas a motor

--- motor.py
+++ motor.py
@@ -1,7 +1,7 @@
-
-
-
 class Motor:
     def __init__(self, marca):
-        self.marca =  marca
-        
+        self.marca = marca
+        self.cilindradas = 0
+
+    def add_cilindradas(self, cilindradas):
+        self.cilindradas = cilindradas
