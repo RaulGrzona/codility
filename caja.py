bcbd818132140a3c369b1a1a796743f57df9f5db
RaulGrzona <raulgrzona@yahoo.com>
2023-05-08 10:05:20 -0300

    cambio automatico

--- caja.py
+++ caja.py
@@ -4,3 +4,9 @@ class CajaCambios:
 
     def __str__(self):
         return f"Esta es una caja con : {self.cantidad_cambios}"
+class Caja_automatica:
+    def __init__(self, cantidad_cambios):
+        self.cantidad_cambios = cantidad_cambios
+
+    def __str__(self):
+        return f"Esta es una caja con : {self.cantidad_cambios}"
