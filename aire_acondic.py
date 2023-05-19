193ed41cd3976c1a9ad98771b9a2e49b85d822f6
RaulGrzona <raulgrzona@yahoo.com>
2023-05-19 15:14:20 -0300

    Agrego cambio cantidad gas refrigerante

--- aire_acondic.py
+++ aire_acondic.py
@@ -3,8 +3,9 @@ class Aire_acondic
         self.refrigerante = refrigerante
         self.tip_refrig = tip_refrig
         self.cantidad_gas = cantidad_gas
-    def cambiar_gas_refrigerante(self, nuevo_gas_refrig):
-        self.nuevo_gas_refrig = nuevo_gas_refrig
 
-        return "r34"
+    def cambiar_cantidad_gas(self, nuevo_cantidad_gas):
+        self.nuevo_cantidad_gas = nuevo_cantidad_gas
+
+        return "800 gr"
 
