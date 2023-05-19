e8fabe6dc245798302f33e31debfca48ff2075bc
RaulGrzona <raulgrzona@yahoo.com>
2023-05-19 15:24:26 -0300

    completo clase tablero

--- tablero.py
+++ tablero.py
@@ -1,1 +1,5 @@
-c
+class Tablero:
+    def __init__(self, velocimetro, cuenta_vueltas, medidor_combustible):
+        self.velocimetro = velocimetro
+        self.cuenta_vueltas = cuenta_vueltas
+        self.medidor_combustible = medidor_combustible
