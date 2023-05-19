ef9b69f122b33c06f05cbf88e2ee1b913f675b03
RaulGrzona <raulgrzona@yahoo.com>
2023-05-19 15:22:37 -0300

    agrego velocimetro a motor

--- motor.py
+++ motor.py
@@ -2,7 +2,8 @@ class Motor:
     def __init__(self, marca):
         self.marca = marca
         self.cilindradas = 0
-
+    def velocimetro(self, velocidad_maxima):
+        self.velocidad_maxima = velocidad_maxima
     def add_cilindradas(self, cilindradas):
         self.cilindradas = cilindradas
     # raul
